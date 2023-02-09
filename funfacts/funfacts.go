bd9c82d3c4fe663bc71d341482a9d11edcc6d078
Sindreln <112351024+Sindreln@users.noreply.github.com>
2023-02-09 19:22:37 +0100

    La til funfacts

--- funfacts/funfacts.go
+++ funfacts/funfacts.go
@@ -1,14 +1,38 @@
 package funfacts
 
-/**
-  Implementer funfacts-funksjon:
-    GetFunFacts(about string) []string
-      hvor about kan ha en av tre testverdier, -
-        sun, luna eller terra
-  Sett inn alle Funfucts i en struktur
-  type FunFacts struct {
-      Sun []string
-      Luna []string
-      Terra []string
-  }
-*/
+type FunFacts struct {
+	Sun   []string
+	Luna  []string
+	Terra []string
+}
+
+func GetFunFacts(about string) []string {
+	funFacts := FunFacts{
+		Sun: []string{
+			"The Sun is over 4.5 billion years old.",
+			"The Sun's light reaches the Earth in eight minutes.",
+			"The Sun is made from Hydrogen and Helium.",
+		},
+		Luna: []string{
+			"The Moon always shows Earth the same face.",
+			"The Moon's surface is actually dark.",
+			"The Moon has quakes too.",
+		},
+		Terra: []string{
+			"Earth has never been perfectly round",
+			"Earth's gravity isn't uniform.",
+			"In the past, sea levels were very different.",
+		},
+	}
+
+	switch about {
+	case "sun":
+		return funFacts.Sun
+	case "luna":
+		return funFacts.Luna
+	case "terra":
+		return funFacts.Terra
+	default:
+		return []string{}
+	}
+}
--- funfacts/funfacts_test.go
+++ funfacts/funfacts_test.go
@@ -14,14 +14,28 @@ import (
 */
 func TestGetFunFacts(t *testing.T) {
 	type test struct {
-		input // her må du skrive riktig type for input
-		want  // her må du skrive riktig type for returverdien
+		input string
+		want  []string
 	}
 
-	// Her må du legge inn korrekte testverdier
-	//tests := []test{
-	//  {input: , want: },
-	//}
+	tests := []test{
+		{input: "sun", want: []string{
+			"The Sun is over 4.5 billion years old.",
+			"The Sun's light reaches the Earth in eight minutes.",
+			"The Sun is made from Hydrogen and Helium.",
+		}},
+		{input: "luna", want: []string{
+			"The Moon always shows Earth the same face.",
+			"The Moon's surface is actually dark.",
+			"The Moon has quakes too.",
+		}},
+		{input: "terra", want: []string{
+			"Earth has never been perfectly round",
+			"Earth's gravity isn't uniform.",
+			"In the past, sea levels were very different.",
+		}},
+		{input: "invalid", want: []string{}},
+	}
 
 	for _, tc := range tests {
 		got := GetFunFacts(tc.input)
